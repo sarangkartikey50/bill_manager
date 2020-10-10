faefb1438f823810b39cdc855b9dd3f4bb03bcd5
sarang kartikey <sarang.kartikey@toppr.com>
2020-10-10 22:31:54 +0530

    Updates

---
+++ static/js/5.23ba80d6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbill_manager=this.webpackJsonpbill_manager||[]).push([[5],{251:function(e,a,t){e.exports={container:"Header_container__7iTTy",title:"Header_title__1F92a"}},543:function(e,a,t){"use strict";t.r(a);var n=t(1),i=t.n(n),r=t(251),c=t.n(r);a.default=Object(n.memo)((function(){return i.a.createElement("div",{className:c.a.container},i.a.createElement("div",{className:c.a.title},"Bill Manager"))}))}}]);
+//# sourceMappingURL=5.23ba80d6.chunk.js.map
